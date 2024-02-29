7a279a342e18d540da0b54c28662449a401585b5
hiraeth31 <beautiful_distress@outlook.com>
2024-02-29 22:46:46 +1000

    hopeless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn
+import seaborn as sns
 
 df = pd.read_csv('obesity.csv')
 
@@ -9,14 +9,14 @@ print(df.info())
 usaData = df[df['Entity'] == 'United States']
 russiaData = df[df['Entity'] == 'Russia']
 
-figure = plt.figure(figsize=(10,5))
+figure = plt.figure(figsize=(10,10))
 figure2 = plt.figure(figsize=(10,10))
+figure3 = plt.figure(figsize=(10,10))
 
-
-axes1 = figure.add_subplot(1,2, 1)
-axes2 = figure.add_subplot(1,2, 2)
+axes1 = figure.add_subplot(2,2, 1)
+axes2 = figure.add_subplot(2,2, 2)
 axes3 = figure2.add_subplot()
-
+axes4 = figure3.add_subplot()
 
 axes3.plot(usaData['Year'], usaData['Indicator:Prevalence of obesity among adults, BMI &GreaterEqual; 30 (crude estimate) (%) - Sex:Both sexes'],
            label='США',
@@ -51,6 +51,44 @@ axes2.set_xlabel('Год')
 axes2.set_ylabel('Ожирение (%)')
 axes2.grid()
 axes2.legend()
+
+russiaData = russiaData.drop(columns=['Code', 'Entity'])
+russiaData = russiaData.rename(columns={"Indicator:Prevalence of obesity among adults, BMI &GreaterEqual; 30 (crude estimate) (%) - Sex:Both sexes": "BMI RUS"}).reset_index()
+print('Данные russiaData ниже:')
+russiaData = russiaData.reset_index(drop=True) # new
+print(russiaData.head(10))
+
+usaData = usaData.drop(columns=['Entity', 'Code', 'Year'])
+usaData = usaData.rename(columns={"Indicator:Prevalence of obesity among adults, BMI &GreaterEqual; 30 (crude estimate) (%) - Sex:Both sexes": "BMI USA"}).reset_index()
+usaData = usaData.reset_index(drop=True) # new
+print('Данные usaData ниже:')
+print(usaData.head(10))
+
+
+hopelessData = pd.concat([russiaData, usaData])
+hopelessData['BMI USA'] = usaData['BMI USA']
+print('Данные ниже с RU and USA:')
+print(hopelessData.head(10))
+
+
+print("Проверка")
+hopelessData = hopelessData.drop(columns=['index'])
+print(hopelessData.info())
+print(hopelessData.head(10))
+corrData = hopelessData.corr()
+print(corrData)
+
+
+# hopelessData.index = pd.MultiIndex.from_tuples(hopelessData.values.tolist(), names=hopelessData.colums) # не работает MultiIndex
+
+
+# print('Данные ниже х2:')
+# print(combined_data.head(10))
+#print("%%%")
+
+# Создание тепловой карты
+sns.heatmap(corrData, annot=True, cmap='coolwarm', fmt=".2f", vmin=-1,vmax=1) # работает, но не так как надо
+axes4.set_title('Корреляция')
 
 
 plt.show()
